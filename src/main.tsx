9f9f8bd920b8fedd448eb91568d847fc940b103c
MmD <mmdj27634@gmail.com>
2024-06-10 22:37:06 +0330

    custom size for buttons in daisyui

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "@/app";
 
 import "@/css/tailwind.css";
+import "@/css/daisyui.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
